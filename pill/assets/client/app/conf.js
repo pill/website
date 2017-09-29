5808fcc89a9ef281111470f553255bbc9da4f1db
Phil <phil.avery@gmail.com>
2017-09-29 08:38:47 -0400

    env vars for webpack

--- pill/assets/client/app/conf.js
+++ pill/assets/client/app/conf.js
@@ -1,11 +1,7 @@
 
-// need flag for this
-const env = 'production'
-
-const host = env == 'development'
+const host = process.env.NODE_ENV === 'development'
                   ? 'http://127.0.0.1:8080'
                   : 'http://phil.avery.nyc'
-
 
 const Conf = {
   host: host,
--- pill/webpack.config.js
+++ pill/webpack.config.js
@@ -18,7 +18,12 @@ var config = {
         loader : 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
 };
 
 module.exports = config;
