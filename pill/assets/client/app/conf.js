b8c01141a7f566dd7bf34acc7f1b9b8341b8848b
Phil <phil.avery@gmail.com>
2017-09-28 22:55:33 -0400

    debug

--- pill/assets/client/app/conf.js
+++ pill/assets/client/app/conf.js
@@ -2,7 +2,7 @@ const env = 'production'
 const host = env == 'development'
                   ? 'http://127.0.0.1:8080'
                   : 'http://phil.avery.nyc'
-
+console.log("host > ", host)
 const Conf = {
   host: host,
   logging: {
